969f14c5c6aa2a5bfac1e05cd8dcaf1f52b04450
Albert <johanein@gmail.com>
2020-04-18 23:12:32 +0530

    added some prop for pagntn boots

--- src/Components/Toast/paginationLib.js
+++ src/Components/Toast/paginationLib.js
@@ -1,12 +1,14 @@
 import React, { memo } from 'react'
 import PaginationComponent from "react-js-pagination";
 import  styles  from './toast.module.css'
-require("bootstrap/less/bootstrap.less");
+// require("bootstrap/less/bootstrap.less");
 
 const Pagination = memo(({postsPerPage,totalPosts,paginate,currentPage})=> {
     return (
         <div  id={styles.paginationstyle} >
         <PaginationComponent
+        itemClass="page-item"
+        linkClass="page-link"
           activePage={currentPage}
           itemsCountPerPage={postsPerPage}
           totalItemsCount={totalPosts}
