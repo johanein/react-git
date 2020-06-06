b6944bce709fc6eed8f1087ecce65fe850d7d66b
Albert <johanein@gmail.com>
2020-06-07 02:43:34 +0530

    added date picker

--- src/Components/AdvanceSearch/mainDisplay.js
+++ src/Components/AdvanceSearch/mainDisplay.js
@@ -4,18 +4,26 @@ import {
     Grid,
 } from '@material-ui/core';
 import { SketchPicker } from 'react-color';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Modal from '../Modal/modal'
 import styles from './advSearch.module.css'
 
 const MainDisplay = memo(()=> {
     const [modalIsOpen,setIsOpen] = useState(false);
     const [color,setColor] = useState('#fff');
+    const [date,setDate] = useState(new Date());
+
     const buttonClick = (params) => ()=>{
         setIsOpen(params)
     }
     const handleColorChangeComplete = (params) => {
         setColor(params.hex)
     }
+
+    const handleDateChange = date => {
+        setDate(date);
+      };
     
 
     return (
@@ -36,8 +44,8 @@ const MainDisplay = memo(()=> {
   alignItems="center"
   >
             <h1>Modal title</h1>
-            <Grid   item
-  direction="row"
+            <Grid   container
+  direction="column"
   justify="center"
   alignItems="center"
   >
@@ -45,8 +53,22 @@ const MainDisplay = memo(()=> {
         color={color}
         onChangeComplete={handleColorChangeComplete}
       />
+  <text>{color}</text>
+      <DatePicker
+  selected={date}
+  //   onSelect={handleSelect} //when day is clicked
+  onChange={handleDateChange} //only when value has changed
+  dateFormat='Pp'
+  filterDate={date=>date.getDay()!==0 && date.getDay()!==6}
+  maxDate={new Date()}
+  isClearable
+  showMonthYearDropdown
+  scrollableMonthYearDropdown
+  showTimeSelect
+  inline
+/>
+  <text>{date.toString()}</text>
             </Grid>
-      <text>{color}</text>
             <Tippy content='close modal'><button onClick={buttonClick(false)} > close model</button></Tippy>
             </Grid>
             </Modal>
