c1a07f6cc098d081589aaecb50ca00564df020c2
Albert <johanein@gmail.com>
2020-09-09 20:10:30 +0530

    Updates

---
+++ precache-manifest.80029fd4ecbceee57ed35480a8d67c2a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "200244c20622651b14cd",
+    "url": "/react-git/static/css/main.9d0196d6.chunk.css"
+  },
+  {
+    "revision": "200244c20622651b14cd",
+    "url": "/react-git/static/js/main.200244c2.chunk.js"
+  },
+  {
+    "revision": "c69cc66f5060fb19ee99",
+    "url": "/react-git/static/js/1.c69cc66f.chunk.js"
+  },
+  {
+    "revision": "890f4ce79429c422d0cc",
+    "url": "/react-git/static/css/2.4cdefa83.chunk.css"
+  },
+  {
+    "revision": "890f4ce79429c422d0cc",
+    "url": "/react-git/static/js/2.890f4ce7.chunk.js"
+  },
+  {
+    "revision": "6cbec0eea9d2f33cc24c",
+    "url": "/react-git/static/js/reactPlayerYouTube.6cbec0ee.chunk.js"
+  },
+  {
+    "revision": "c3652d65e48fd5bba64c",
+    "url": "/react-git/static/js/reactPlayerSoundCloud.c3652d65.chunk.js"
+  },
+  {
+    "revision": "26a88100f07e45774eea",
+    "url": "/react-git/static/js/reactPlayerVimeo.26a88100.chunk.js"
+  },
+  {
+    "revision": "b7a7953b565387d9a597",
+    "url": "/react-git/static/js/reactPlayerFacebook.b7a7953b.chunk.js"
+  },
+  {
+    "revision": "0dfdeab8abe15e138642",
+    "url": "/react-git/static/js/reactPlayerStreamable.0dfdeab8.chunk.js"
+  },
+  {
+    "revision": "746faf0850cad131409f",
+    "url": "/react-git/static/js/reactPlayerWistia.746faf08.chunk.js"
+  },
+  {
+    "revision": "a550146259a16d52476a",
+    "url": "/react-git/static/js/reactPlayerTwitch.a5501462.chunk.js"
+  },
+  {
+    "revision": "eb5afbca2985abebafd0",
+    "url": "/react-git/static/js/reactPlayerDailyMotion.eb5afbca.chunk.js"
+  },
+  {
+    "revision": "caf9f7b2501e090617e8",
+    "url": "/react-git/static/js/reactPlayerMixcloud.caf9f7b2.chunk.js"
+  },
+  {
+    "revision": "c18196b384e708391cec",
+    "url": "/react-git/static/js/reactPlayerVidyard.c18196b3.chunk.js"
+  },
+  {
+    "revision": "178bbd8fa74e973842d1",
+    "url": "/react-git/static/js/reactPlayerFilePlayer.178bbd8f.chunk.js"
+  },
+  {
+    "revision": "be5c995616370447659a",
+    "url": "/react-git/static/js/reactPlayerPreview.be5c9956.chunk.js"
+  },
+  {
+    "revision": "dc22e5540936a531e54a",
+    "url": "/react-git/static/css/15.9d05f835.chunk.css"
+  },
+  {
+    "revision": "dc22e5540936a531e54a",
+    "url": "/react-git/static/js/15.dc22e554.chunk.js"
+  },
+  {
+    "revision": "7d5a9000d0213a76b49d",
+    "url": "/react-git/static/js/16.7d5a9000.chunk.js"
+  },
+  {
+    "revision": "3130184db809c0159244",
+    "url": "/react-git/static/js/17.3130184d.chunk.js"
+  },
+  {
+    "revision": "f39839df074540bb0f56",
+    "url": "/react-git/static/js/runtime~main.f39839df.js"
+  },
+  {
+    "revision": "acf8251dadba85be7e9e8637fbc0d393",
+    "url": "/react-git/static/media/bg.acf8251d.jpg"
+  },
+  {
+    "revision": "cfbe3324746ea6443a47429f681fee37",
+    "url": "/react-git/static/media/EMTab_background.cfbe3324.jpg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "8c26aafbf63d29fef454d5e81e636c4a",
+    "url": "/react-git/index.html"
+  }
+];
