15d6d58fea9316770308d5d372b31fbf0ea49c5c
Albert <johanein@gmail.com>
2020-11-12 12:49:34 +0530

    nav style to module

--- src/Nav/nav.js
+++ src/Nav/nav.js
@@ -4,13 +4,14 @@ import {
   } from 'react-router-dom'
 import {  useSelector  } from 'react-redux'
   import navItems from './navItems'
+  import style from "./nav.module.css";
 
 const Nav = memo( (props)=>{
   const loginState = useSelector(state => state.logoutReducer.loginState)
    if(loginState) 
     return (
         <nav >
-        <ul className='Nav'>
+        <ul className={style.nav}>
           {navItems.map(({url,name})=>(
             <li key = {url}>
             <Link to={url}>{name}</Link>
