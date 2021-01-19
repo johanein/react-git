021940a01148cd80a6a11204c5b630e0619cdea8
Albert <johanein@gmail.com>
2021-01-19 22:35:31 +0530

    temp commit for ajax call in login

--- src/Components/Logout/logout.js
+++ src/Components/Logout/logout.js
@@ -1,25 +1,44 @@
-import React, { memo,useEffect } from 'react'
+import React, { memo,useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import styles from './logout.module.css'
 import { logoutActionCreator } from '../../Redux/actions/logoutAction'
 
 const Logout = memo((props)=>{
-    const dispatch = useDispatch()
-    let history = useHistory();
+    const dispatch = useDispatch();
+    const [formDatas, setFormDatas] = useState({
+        username:'',
+        password:'',
+    })
+    // let history = useHistory();
+
     useEffect(() => {
         dispatch(logoutActionCreator(false))
-    }, [])
-    const handleOnClick = (params) => {
-        dispatch(logoutActionCreator(true))
-        history.push("/home");
+    }, []);
+
+    const handleOnchange = (event) => {
+        const {name, value} = event.target || {}
+        setFormDatas((s) => ({
+            ...s,
+            [name]:value
+        }));
     }
+    
+    const handleOnSubmit = (event) => {
+        console.log('formDatas :', formDatas);
+        event.preventDefault();
+        // dispatch(logoutActionCreator(true))
+        // history.push("/home");
+    };
     
     return (
         <div className={styles.logoutMain}>
-            <div> username <input/></div>
-            <div> password <input type='password'/></div><br/>
-            <div onClick={handleOnClick}><button type='submit'>submit</button></div>
+            <form onSubmit={handleOnSubmit}>
+            <input type='text' name='username' onChange={handleOnchange}/>
+            <input type='password' name='password' onChange={handleOnchange}/>
+            <button type='submit'>submit</button>
+            </form>
+            {/* <div onClick={handleOnSubmit}></div> */}
         </div>
     )
 })
