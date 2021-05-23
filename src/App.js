4f81cb1611e4bb72eb4e340078ffc81d985acd56
Albert <johanein@gmail.com>
2021-05-23 23:46:40 +0530

    small prettier changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React, { memo } from 'react'
-import { Provider } from 'react-redux'
-import {
-  Route,
-  Switch,
-  BrowserRouter as Router
-} from 'react-router-dom'
-import 'tippy.js/dist/tippy.css'; 
-import 'react-toastify/dist/ReactToastify.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import store from './Redux/store'
-import IdleTimeComp from './Components/IdleTimer/idleTimer'
-import Nav from './Nav/nav'
-import navItems from './Nav/navItems'
-import ErrorBoundary from './Components/ErrorBoundary'
+import React, { memo } from "react";
+import { Provider } from "react-redux";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import "tippy.js/dist/tippy.css";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import store from "./Redux/store";
+import IdleTimeComp from "./Components/IdleTimer/idleTimer";
+import Nav from "./Nav/nav";
+import navItems from "./Nav/navItems";
+import ErrorBoundary from "./Components/ErrorBoundary";
 
 const Routes = () => {
-  return navItems.map(({url,component})=>(
+  return navItems.map(({ url, component }) => (
     <Route exact key={url} path={`/${url}`}>
-<ErrorBoundary>
-    {component}
-</ErrorBoundary>
-  </Route>
-  ))
-}
+      <ErrorBoundary>{component}</ErrorBoundary>
+    </Route>
+  ));
+};
 
-const App = memo(function App(props) {
+const App = memo((props) => {
   return (
     <Provider store={store}>
-    <div className='App' >
-      < Router >
-      <IdleTimeComp/>
-        <Nav/>
-        <Switch>
-<Routes/>
-        </Switch>
-      </Router>
-    </div>
-  </Provider>
-  )
-})
-export default App
+      <div className="App">
+        <Router>
+          <IdleTimeComp />
+          <Nav />
+          <Switch>
+            <Routes />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
+  );
+});
+export default App;
